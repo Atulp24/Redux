cd46dfc4cf2c1d508aa611772737da05c45cb171
atul pandey <apk200209@gmail.com>
2025-06-11 16:45:26 +0530

    Commit

---
+++ Day7/funStuff/app.js
@@ -0,0 +1,27 @@
+// import chalk from "chalk";
+// import figlet from "figlet";
+// import catMe from "cat-me";
+// import cowsay from "cowsay";
+// import ft from "fortune-teller";
+
+// console.log(chalk.red("This is a red colored text"));
+// console.log(chalk.blue("This is a blue colored text"));
+// console.log(chalk.black("This is a black colored text"));
+
+// figlet("Hello ji", (err, data) => {
+//     console.log(data)
+// });
+
+// figlet("Atul Pandey", (err, data) => {
+//     console.log(chalk.red(data))
+// });
+
+// console.log(catMe());
+
+// console.log(chalk.red(cowsay.say({
+//     text: "I'm a mooodule",
+//     e: "vv",
+//     T: "U"
+// })));
+
+// console.log(ft.fortune());
